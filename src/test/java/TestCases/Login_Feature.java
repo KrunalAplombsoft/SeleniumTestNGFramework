77929ec0a08c464db8b94f324ef10914e5acf6be
ASPL-LP008-User <krunal.parekh@aplombsoft.com>
2024-01-10 17:59:52 +0530

    Updated XML

--- src/test/java/TestCases/Login_Feature.java
+++ src/test/java/TestCases/Login_Feature.java
@@ -1,14 +1,14 @@
 package TestCases;
 
 import Utilities.BaseClass;
-import WebAppFunctions.Login_Test_Steps;
 import org.testng.annotations.Test;
 
 public class Login_Feature extends BaseClass {
 
-    @Test
+    @Test(groups = {"Smoke"})
     public void Login_User_With_Correct_Email_And_Password()
     {
+        System.out.println("Smoke Test");
 //        Login_Test_Steps loginTestSteps = new Login_Test_Steps(webDriver);
 //        loginTestSteps.openLoginPage();
 //        loginTestSteps.enterLoginCredentials();
@@ -16,15 +16,16 @@ public class Login_Feature extends BaseClass {
 //        loginTestSteps.deleteAccount();
     }
 
-//    @Test
-//    public void Login_User_With_Incorrect_Email_And_Password()
-//    {
+    @Test(groups = {"Sanity"})
+    public void Login_User_With_Incorrect_Email_And_Password()
+    {
+        System.out.println("Sanity Test");
 //        Login_Test_Steps loginTestSteps = new Login_Test_Steps(webDriver);
 //        loginTestSteps.openLoginPage();
 //        loginTestSteps.enterLoginCredentials();
 //        loginTestSteps.verifyAfterLoginUser();
 //        loginTestSteps.deleteAccount();
-//    }
+    }
 //    @Test
 //    public void Logout_User()
 //    {
