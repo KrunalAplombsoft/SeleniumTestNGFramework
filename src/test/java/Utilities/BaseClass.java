d7fcf9ca04aa199d19148a7b9aa7e93622902fa5
ASPL-LP008-User <krunal.parekh@aplombsoft.com>
2024-01-11 15:06:27 +0530

    Cod Updated

--- src/test/java/Utilities/BaseClass.java
+++ src/test/java/Utilities/BaseClass.java
@@ -16,7 +16,7 @@ public class BaseClass {
 
     public static WebDriver webDriver;
 
-    @BeforeTest(groups = {"Web Smoke","Web Sanity"})
+    @BeforeTest(groups = {"WebSmoke","WebSanity"})
     public void first() throws IOException
     {
         FileInputStream fileInputStream = new FileInputStream("./config.properties");
