8ed5d4cc989b488a63ced7ab58011bdada234c79
ASPL-LP008-User <krunal.parekh@aplombsoft.com>
2024-01-11 14:37:45 +0530

    Cod Updated

--- src/test/java/TestCases/Login_Feature.java
+++ src/test/java/TestCases/Login_Feature.java
@@ -6,13 +6,13 @@ import org.testng.annotations.Test;
 
 public class Login_Feature extends BaseClass {
 
-    @Test(groups = {"Web","Smoke","CorrectEmail"})
+    @Test(groups = {"Smoke","CorrectEmail"})
     public void Login_User_With_Correct_Email_And_Password()
     {
         System.out.println("Smoke Test");
     }
 
-    @Test(groups = {"Web","Sanity","IncorrectEmail"})
+    @Test(groups = {"Sanity","IncorrectEmail"})
     public void Login_User_With_Incorrect_Email_And_Password()
     {
         System.out.println("Sanity Test");
