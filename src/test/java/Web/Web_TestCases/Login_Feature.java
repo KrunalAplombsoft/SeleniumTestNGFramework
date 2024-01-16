9329046dacf08e23696c5eca8f67acbe2c64fe71
ASPL-LP008-User <krunal.parekh@aplombsoft.com>
2024-01-16 18:13:22 +0530

    Code Updated

--- src/test/java/Web/Web_TestCases/Login_Feature.java
+++ src/test/java/Web/Web_TestCases/Login_Feature.java
@@ -6,12 +6,12 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Login_Feature extends WebBaseClass {
-    
-    @Parameters({"name"})
+
+    @Parameters({"name","surname"})
     @Test(groups = {"Web","Smoke"})
-    public void Login_User_With_Correct_Email_And_Password(String name)
+    public void Login_User_With_Correct_Email_And_Password(String name, String surname)
     {
-        System.out.println("Smoke Test"+name);
+        System.out.println("Smoke Test " + name + " " + surname);
     }
 
     @Test(groups = {"Web","Sanity"})
